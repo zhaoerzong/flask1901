6fc135a0be91a9626b88485bad9428d327841763
cong <1836548901@qq.com>
2020-04-01 17:22:29 +0800

    Flask模板完成

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
+
+#多URL
 @app.route('/')
+# @app.route('/index')
+# @app.route('/home')
 
 def index():
+    name = "大聪"
+    movies = [
+        {'title':'杀破狼','year':'2010'},
+        {'title':'唐人街探案','year':'2015'},
+        {'title':'战狼2','year':'2017'},
+        {'title':'我不是药神','year':'2018'},
+        {'title':'复仇者联盟4','year':'2019'},
+        {'title':'大赢家','year':'2020'},
+    ]
+    return render_template('index.html',name=name,movies=movies)
+    # return "<h1>hello,Flask!!!</h1>"
 
-    return "<h1>hello,Flask!!!</h1>"
+
+#动态URL
+# @app.route('/index/<name>')
+# def home(name):
+#     print(url_for("home",name='Cong'))
+#     return "<h1>hello,%s!!!</h1>"%name
