5c8f41118cd0dfed3e910761c48d33d1ab4a7ad4
cong <1836548901@qq.com>
2020-04-02 13:42:38 +0800

    jinjia2模板继承+模板上下文处理函数

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -65,16 +65,34 @@ def forge():
 
 #views
 #多URL
+
 @app.route('/')
 # @app.route('/index')
 # @app.route('/home')
-
+#首页
 def index():
 
-    user = User.query.first()   #读取用户记录
+    # user = User.query.first()   #读取用户记录
     movies = Movie.query.all()  #读取所有电影记录
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
     # return "<h1>hello,Flask!!!</h1>"
+
+#处理页面404错误
+@app.errorhandler(404)
+def page_not_found(e):
+    # user = User.query.first()
+    return render_template('404.html'),404
+
+
+
+#模板上下文处理函数,在多个模板内都需要使用的变量
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+
 
 
 #动态URL
